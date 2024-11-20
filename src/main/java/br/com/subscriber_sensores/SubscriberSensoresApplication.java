a0ef360999fb38f3deb13c64179a4f8c9b9f1a81
Eduardo Fontenele <eduardofontedev@gmail.com>
2024-11-20 19:19:46 -0300

    commit inicial

---
+++ src/main/java/br/com/subscriber_sensores/SubscriberSensoresApplication.java
@@ -0,0 +1,13 @@
+package br.com.subscriber_sensores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriberSensoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriberSensoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/subscriber_sensores/SubscriberSensoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.subscriber_sensores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriberSensoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
